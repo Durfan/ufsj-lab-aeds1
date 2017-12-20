bf17549f4cbb84cec8a2f089ac307f856648337e
Durfan <31577607+Durfan@users.noreply.github.com>
2017-12-20 21:59:05 -0200

    patch 5.5

--- lista05/prob5.c
+++ lista05/prob5.c
@@ -9,27 +9,16 @@
 
 int main() {
     int i, neg=0;
-<<<<<<< HEAD
     float vt[10], soma=0;
     
     for (i=0;i<10;i++){
         printf("vt[%d]=", i);
-=======
-    float vt[10], soma;
-    
-    for (i=0;i<10;i++){
-        printf("vt[%d]=", i+1);
->>>>>>> 9b61ec32d1decacc1c15f26dc11d9501781bbaf8
         scanf("%f", &vt[i]);
     }
     
     for (i=0;i<10;i++) {
         soma+=vt[i];
-<<<<<<< HEAD
         printf("+(%f)", vt[i]);
-=======
-        printf("+%f", vt[i+1]);
->>>>>>> 9b61ec32d1decacc1c15f26dc11d9501781bbaf8
         if (vt[i]<0){
             neg++;
         }
