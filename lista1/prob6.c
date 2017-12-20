140413c605a156b31b09ecd12a793189ef5c28cd
Pablo Cecilio <31577607+Durfan@users.noreply.github.com>
2017-12-20 10:23:35 -0200

    Update prob6.c

--- lista1/prob6.c
+++ lista1/prob6.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-char c;
+  
+  char c;
 
-printf ("Char: ");
-scanf ("%c", &c);
+  printf ("Char: ");
+  scanf ("%c", &c);
+  printf ("Leitura %i\n", c);
 
-printf ("Leitura %i\n", c);
-
-return 0;
+  return 0;
 }
