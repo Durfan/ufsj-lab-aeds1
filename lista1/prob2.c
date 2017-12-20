5c4dda67d6057c2ae6d179460dc2b7f65f136b4c
Pablo Cecilio <31577607+Durfan@users.noreply.github.com>
2017-12-20 10:21:55 -0200

    Update prob2.c

--- lista1/prob2.c
+++ lista1/prob2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-int num;
 
-printf ("Valor: ");
-scanf ("%i", &num);
+  int num;
 
-printf ("Valor lido: %i\n", num);
+  printf ("Valor: ");
+  scanf ("%i", &num);
+  printf ("Valor lido: %i\n", num);
 
   return 0;
 }
