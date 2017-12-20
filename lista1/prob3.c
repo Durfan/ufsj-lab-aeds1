df69d18a55d34507d8f14e4548c7f961df7b4d79
Pablo Cecilio <31577607+Durfan@users.noreply.github.com>
2017-12-20 10:22:32 -0200

    Update prob3.c

--- lista1/prob3.c
+++ lista1/prob3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-int num;
 
-printf ("Valor: ");
-scanf ("%i", &num);
-
-printf ("Valor lido: %f\n", num);
-
+  int num;
+  
+  printf ("Valor: ");
+  scanf ("%i", &num);
+  printf ("Valor lido: %f\n", num);
+  
   return 0;
 }
